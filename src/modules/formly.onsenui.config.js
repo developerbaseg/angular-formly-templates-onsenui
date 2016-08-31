0180f351612bb0a73706a09bea60149b0f6c812a
Base Global I+D <developer@baseglobal.com.ar>
2016-08-31 11:04:36 -0300

    Initial commit
    v1.0

---
+++ src/modules/formly.onsenui.config.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    angular.module('formly.onsenui').config(configFormlyOnsenui);
+    
+    function configFormlyOnsenui(formlyConfigProvider) {
+        
+        angular.forEach(['list-section-title','list-input','list-switch','list-range','list-radio','list-checkbox','list-textarea','list-select','list-selector'], function (fieldName) {
+        formlyConfigProvider.setType({
+                name: fieldName,
+                templateUrl: getFieldTemplateUrl(fieldName)
+            });
+        });
+        
+        function getFieldTemplateUrl(name) {
+            return 's2/onsenui/fields/ons-' + name + '.html';
+        }
+
+    }
+})();
